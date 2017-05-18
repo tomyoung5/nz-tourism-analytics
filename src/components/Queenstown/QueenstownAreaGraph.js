4e466c8e320268dc52cbdb62fe0547af217d31d8
tom <tom@csvue.com>
2017-05-19 09:43:55 +1200

    Reference line

--- src/components/Queenstown/QueenstownAreaGraph.js
+++ src/components/Queenstown/QueenstownAreaGraph.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Area, AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ReferenceLine } from 'recharts';
 
 import { queenstownData } from '../../data/queenstown-accomodation.js';
 
@@ -12,12 +12,13 @@ class QueenstownAreaGraph extends React.Component{
     render() {
         return (
             <AreaChart width={1000} height={600} data={queenstownData}
-                margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+                margin={{top: 20, right: 30, left: 0, bottom: 0}}>
                 <XAxis dataKey="year"/>
                 <YAxis width={70}/>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Tooltip wrapperStyle={{backgroundColor: '#303030'}}/>
-                <Legend/>                            
+                <Legend/>
+                <ReferenceLine x="2016" stroke="red" label="Future Prediction" />
                 <Area type='monotone' dataKey='hotel' name="Hotel" stackId="1" stroke='#8884d8' fill='#8884d8' />
                 <Area type='monotone' dataKey='familyAndFriends' name="Family and Friends" stackId="1" stroke='#82ca9d' fill='#82ca9d' />
                 <Area type='monotone' dataKey='motel' name="Motel" stackId="1" stroke='#ffc658' fill='#ffc658' />
--- src/components/Queenstown/QueenstownLineGraph.js
+++ src/components/Queenstown/QueenstownLineGraph.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ReferenceLine } from 'recharts';
 
 import { queenstownData } from '../../data/queenstown-accomodation.js';
-
-
 
 
 class QueenstownLineGraph extends React.Component{
@@ -12,20 +10,21 @@ class QueenstownLineGraph extends React.Component{
     render() {
         return (
             <LineChart  width={1000} height={600} data={queenstownData}
-                margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+                margin={{top: 20, right: 30, left: 0, bottom: 0}}>
                 <XAxis dataKey="year"/>
                 <YAxis width={70}/>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Tooltip wrapperStyle={{backgroundColor: '#303030'}}/>
                 <Legend/>
-                <Line type='monotone' dataKey='total' name="Total" stroke='#fff' />
-                <Line type='monotone' dataKey='hotel' name="Hotel" stroke='#8884d8' />
-                <Line type='monotone' dataKey='familyAndFriends' name="Family and Friends" stroke='#82ca9d' />
-                <Line type='monotone' dataKey='motel' name="Motel" stroke='#ffc658' />
-                <Line type='monotone' dataKey='camping' name="Camping" stroke='#8884d8' />
-                <Line type='monotone' dataKey='backpackers' name="Backpackers" stroke='#82ca9d' />
-                <Line type='monotone' dataKey='luxury' name="Luxury Accomodation" stroke='#ffc658' />
-                <Line type='monotone' dataKey='bAndB' name="B & B" stroke='#8884d8' />
+                <ReferenceLine x="2016" stroke="red" label="Future Prediction" />
+                <Line type='monotone' dataKey='total' name="Total" dot={false} stroke='#fff' />
+                <Line type='monotone' dataKey='hotel' name="Hotel" dot={false} stroke='#8884d8' />
+                <Line type='monotone' dataKey='familyAndFriends' name="Family and Friends" dot={false} stroke='#82ca9d' />
+                <Line type='monotone' dataKey='motel' name="Motel" dot={false} stroke='#ffc658' />
+                <Line type='monotone' dataKey='camping' name="Camping" dot={false} stroke='#8884d8' />
+                <Line type='monotone' dataKey='backpackers' name="Backpackers" dot={false} stroke='#82ca9d' />
+                <Line type='monotone' dataKey='luxury' name="Luxury Accomodation" dot={false} stroke='#ffc658' />
+                <Line type='monotone' dataKey='bAndB' name="B & B" dot={false} stroke='#8884d8' />
             </LineChart >
         )
     }
